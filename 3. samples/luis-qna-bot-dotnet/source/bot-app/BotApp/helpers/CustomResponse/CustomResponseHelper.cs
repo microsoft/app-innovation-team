b988b30a1b08a1cdb4468ff3db9a6b7d21e5a997
Roberto Cervantes <rcervantes@outlook.com>
2019-04-04 15:26:47 -0600

    Adding custom response feature to bot app

---
+++ 3. samples/luis-qna-bot-dotnet/source/bot-app/BotApp/helpers/CustomResponse/CustomResponseHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Bot.Builder;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BotApp
+{
+    public class CustomResponseHelper : BaseHelper
+    {
+        private ICustomResponse customResponse = null;
+
+        public CustomResponseHelper()
+        {
+        }
+
+        public async Task SendActivityAsync(ITurnContext context, CancellationToken cancellationToken, string responseType, string message)
+        {
+            switch (responseType)
+            {
+                case "text":
+                    this.customResponse = new TextCustomResponse();
+                    break;
+
+                case "video":
+                    this.customResponse = new VideoCardCustomResponse();
+                    break;
+
+                default:
+                    this.customResponse = new TextCustomResponse();
+                    message = "There was an error identifying the metadata tag";
+                    break;
+            }
+
+            await this.customResponse.SendActivityAsync(context, cancellationToken, message);
+        }
+    }
+}
