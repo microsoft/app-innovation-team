144b3cca58f3504fcb40becd40c45dab645cf097
Nate Rickard <NateRickard@gmail.com>
2019-04-23 17:18:57 -0400

    updated all dependencies to latest

--- 2. solutions/resource-finder/source/Mobile/Android/app/src/main/java/com/azure/mobile/resources/MainActivity.kt
+++ 2. solutions/resource-finder/source/Mobile/Android/app/src/main/java/com/azure/mobile/resources/MainActivity.kt
@@ -2,7 +2,7 @@ package com.azure.mobile.resources
 
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 
@@ -11,7 +11,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
@@ -22,16 +24,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
+
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
