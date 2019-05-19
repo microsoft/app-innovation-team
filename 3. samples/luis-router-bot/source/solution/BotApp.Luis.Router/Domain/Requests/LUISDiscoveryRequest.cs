2bb29a91b3c0ac4bab7b5738724821ebf238df63
Roberto Cervantes <robece@protonmail.com>
2019-05-19 18:58:13 -0500

    Removing old version of luis router bot app

--- 3. samples/luis-router-bot/source/solution/BotApp.Luis.Router/Domain/Requests/LUISDiscoveryRequest.cs
+++
@@ -1,7 +0,0 @@
-namespace BotApp.Luis.Router.Domain.Requests
-{
-    public class LUISDiscoveryRequest
-    {
-        public string Text { get; set; }
-    }
-}
--- 3. samples/luis-router-bot/source/solution/BotApp/entities/LuisAppDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BotApp
-{
-    public class LuisAppDetail
-    {
-        public string Name { get; set; }
-        public string Intent { get; set; }
-        public double Score { get; set; }
-    }
-}
