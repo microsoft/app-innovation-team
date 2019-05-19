00b7a8f5e73d0d710bb68edf2dacd5dfeed8e330
Roberto Cervantes <rcervantes@tutanota.com>
2019-05-19 18:58:37 -0500

    Luis router bot sample (#13)
    * Adding luis router sample
    
    * Delete BotApp-QnA.tsv
    
    * Adding custom extensions to luis router bot app

---
+++ 3. samples/luis-router-bot/source/solution/BotApp.Extensions.BotBuilder.LuisRouter/Accessors/LuisRouterAccessor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.AI.Luis;
+using System;
+using System.Collections.Generic;
+
+namespace BotApp.Extensions.BotBuilder.LuisRouter.Accessors
+{
+    public class LuisRouterAccessor
+    {
+        public LuisRouterAccessor(UserState userState, Dictionary<string, LuisRecognizer> luisServices)
+        {
+            UserState = userState ?? throw new ArgumentNullException(nameof(userState));
+            LuisServices = luisServices ?? throw new ArgumentNullException(nameof(luisServices));
+        }
+
+        public UserState UserState { get; }
+        public IStatePropertyAccessor<string> TokenPreference { get; set; }
+        public Dictionary<string, LuisRecognizer> LuisServices { get; }
+    }
+}
---
+++ 3. samples/luis-router-bot/source/solution/BotApp.Luis.Router.Identity/Controllers/IdentityController.cs
@@ -0,0 +1,127 @@
+﻿using BotApp.Extensions.Common.KeyVault.Helpers;
+using BotApp.Luis.Router.Identity.Domain.Enums;
+using BotApp.Luis.Router.Identity.Domain.Exceptions;
+using BotApp.Luis.Router.Identity.Domain.Requests;
+using BotApp.Luis.Router.Identity.Domain.Responses;
+using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotApp.Luis.Router.Identity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly TelemetryClient telemetry = null;
+        private readonly ILogger logger = null;
+
+        public IdentityController(TelemetryClient telemetry, ILogger<IdentityController> logger)
+        {
+            this.telemetry = telemetry;
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]IdentityRequest model)
+        {
+            this.telemetry.TrackEvent("Identity Post");
+
+            // non-forced-to-disposal
+            string response = string.Empty;
+            IdentityResponse result = new IdentityResponse
+            {
+                IsSucceded = true,
+                ResultId = (int)IdentityResultEnum.Success
+            };
+
+            // forced-to-disposal
+            SymmetricSecurityKey secretKey = null;
+            SigningCredentials signinCredentials = null;
+
+            try
+            {
+                if (string.IsNullOrEmpty(model.AppIdentity))
+                    throw new BusinessException((int)IdentityResultEnum.FailedEmptyAppIdentity);
+
+                string encryptionkey = string.Empty;
+                string appcode = string.Empty;
+                using (KeyVaultHelper keyVaultHelper = new KeyVaultHelper(Startup.EnvironmentName, Startup.ContentRootPath))
+                {
+                    encryptionkey = await keyVaultHelper.GetVaultKeyAsync(Settings.KeyVaultEncryptionKey);
+                    appcode = await keyVaultHelper.GetVaultKeyAsync(Settings.KeyVaultApplicationCode);
+                }
+
+                if (string.IsNullOrEmpty(appcode))
+                    throw new BusinessException((int)IdentityResultEnum.FailedEmptyServerApplicationCode);
+
+                var decrypted = string.Empty;
+                decrypted = NETCore.Encrypt.EncryptProvider.AESDecrypt(model.AppIdentity, encryptionkey);
+
+                dynamic data = JsonConvert.DeserializeObject(decrypted);
+                string decryptedAppCode = data?.appcode;
+
+                if (appcode != decryptedAppCode)
+                    throw new BusinessException((int)IdentityResultEnum.FailedIncorrectCredentials);
+
+                DateTime when = data?.timestamp;
+                if (when < DateTime.UtcNow.AddMinutes(-5))
+                    return Unauthorized();
+
+                secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Settings.AuthorizationKey));
+                signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: "https://BotApp.Luis.Router.Identity",
+                    audience: "https://BotApp.Luis.Router.Identity",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: signinCredentials
+                );
+
+                response = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            }
+            catch (Exception ex)
+            {
+                result.IsSucceded = false;
+
+                if (ex is BusinessException)
+                {
+                    result.ResultId = ((BusinessException)ex).ResultId;
+                }
+                else
+                {
+                    result.ResultId = (int)IdentityResultEnum.Failed;
+
+                    this.logger.LogError($">> Exception: {ex.Message}, StackTrace: {ex.StackTrace}");
+
+                    if (ex.InnerException != null)
+                    {
+                        this.logger.LogError($">> Inner Exception Message: {ex.InnerException.Message}, Inner Exception StackTrace: {ex.InnerException.StackTrace}");
+                    }
+                }
+            }
+            finally
+            {
+                // clean forced-to-disposal
+                secretKey = null;
+                signinCredentials = null;
+
+                GC.Collect();
+            }
+
+            string message = EnumDescription.GetEnumDescription((IdentityResultEnum)result.ResultId);
+            this.logger.LogInformation($">> Message information: {message}");
+
+            return (result.IsSucceded) ? (ActionResult)new OkObjectResult(new { token = response }) : (ActionResult)new BadRequestObjectResult(new { message = message });
+        }
+    }
+}
